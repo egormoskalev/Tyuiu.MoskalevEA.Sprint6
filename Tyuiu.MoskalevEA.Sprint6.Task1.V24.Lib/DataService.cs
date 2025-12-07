9ab71809fd71bfc5185cef89fd6a2c3804fd8567
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 21:33:35 +0500

    Task1

---
+++ Tyuiu.MoskalevEA.Sprint6.Task1.V24.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MoskalevEA.Sprint6.Task1.V24.Lib
+{
+    public class DataService : ISprint6Task1V24
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++) count++;
+
+            double[] mass = new double[count];
+            int k = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == -1.7)
+                {
+                    mass[k] = 0;
+                    continue;
+                }
+
+                mass[k] = Math.Round(Math.Sin(x) / (x + 1.7) - Math.Cos(x) * (4 * x) - 6, 2);
+                k++;
+            }
+            return mass;
+        }
+    }
+}
---
+++ Tyuiu.MoskalevEA.Sprint6.Task1.V24.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint6.Task1.V24.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
