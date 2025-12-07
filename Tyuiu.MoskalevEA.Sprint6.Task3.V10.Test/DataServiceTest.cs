98a5565342e88a9f0d80427305c3b376ef859959
Егор Москалев <moskalevegor310@gmail.com>
2025-12-08 00:20:23 +0500

    Task3

---
+++ Tyuiu.MoskalevEA.Sprint6.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint6.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
