574b87728f054f0d5a095ce78ccc76baf9dd5478
Егор Москалев <moskalevegor310@gmail.com>
2025-12-07 21:24:51 +0500

    Add project files.

---
+++ Tyuiu.MoskalevEA.Sprint6.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MoskalevEA.Sprint6.Task0.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
